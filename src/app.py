7cb4fd8cead47d8d3128c5be1b510af64397f710
Himanshu <himanshu@prevalonenergy.com>
2024-06-03 11:16:00 -0400

    Added Cost Memo

--- src/app.py
+++ src/app.py
@@ -251,28 +251,6 @@ def calculation(proj_location, proj_name, power_req, duration, number_cycles, po
                                "Quantities (#)" : [optimized_number_of_stacks, optimized_number_of_containers, optimized_number_of_pcs]}) 
 
 
-    #bol_design_table = pd.DataFrame({})
-
-    # bol_config.loc[3, "Parameter"] = "Months from FAT to COD"
-    # bol_config.loc[3, "Quantities (#)"] = months_to_COD
-    
-    # bol_config.loc[4, "Parameter"] = "BESS Peak Aux Power (kW)"
-    # bol_config.loc[4, "Quantities (#)"] = '{:,.2f}'.format(aux_power*1000)
-    
-    # bol_config.loc[5, "Parameter"] = "Aux Energy during discharge (kWh)"
-    # bol_config.loc[5, "Quantities (#)"] = '{:,.2f}'.format(aux_energy_per_stack*optimized_number_of_stacks)
-
-
-    financial_table = pd.DataFrame({})
-    financial_table["Component"] = ["Stacks | "+str(math.ceil(batt_nameplate)*optimized_number_of_stacks) + " kWh", str(battery_model) + " Containers", "PCS | " + str(PCS_model)]
-
-    financial_table["Per Unit Cost ($)"] =['${:,.2f}'.format(cost_stack), '${:,.2f}'.format(cost_container), '${:,.2f}'.format(cost_pcs)]
-
-    financial_table["Total Cost per component ($)"] =[ '${:,.2f}'.format(math.ceil(optimized_number_of_stacks*cost_stack)), '${:,.2f}'.format(math.ceil(optimized_number_of_containers*cost_container)), '${:,.2f}'.format(math.ceil(optimized_number_of_pcs*cost_pcs))]
-
-    financial_table.loc[3, "Component"] = "Total Equipment Cost Ex-Works ($)"
-    financial_table.loc[3, "Total Cost per component ($)"] = '${:,.2f}'.format(math.ceil(optimized_number_of_stacks*cost_stack) + math.ceil(optimized_number_of_containers*cost_container) + math.ceil(optimized_number_of_pcs*cost_pcs))
-
     ## Power - Energy Table
     power_energy_table = pd.DataFrame(optimized_number_of_stacks*batt_usable_ac*get_deg_curve(battery_model,number_cycles,duration,r_SOC)["Degradation Curve"])
     power_energy_table = power_energy_table.rename(columns={"Degradation Curve": 'Net Energy @ BOL at '+ str(point_of_measurement)+ ' (kWh)'})
@@ -419,7 +397,31 @@ def calculation(proj_location, proj_name, power_req, duration, number_cycles, po
     bol_design_summary["Value"] = '{:,.0f}'.format(optimized_number_of_containers), '{:,.0f}'.format(optimized_number_of_pcs), '{:,.2f}'.format(aux_power*1000), \
                                 '{:,.2f}'.format(aux_energy_per_stack*optimized_number_of_stacks) , '{:,.2f}'.format((float(power_energy_rte_table["Usable AC Energy at POM (MWh)"][0].replace(',', ''))*1000))
 
+
+#--------------------------------------------------- Cost Memo
+
+    number_of_strings_per_PCS = optimized_number_of_stacks/optimized_number_of_pcs
+
+    i = stacks_config.index(int(number_of_strings_per_PCS))
+
+    container_config_8_strings =  [1, 0, 0, 2, 0, 0, 2, 1, 0, 0, 0, 0, 0, 0, 0]
+    container_config_10_strings = [0, 1, 0, 0, 2, 0, 1, 2, 3, 2, 1, 0, 4, 2, 0]
+    container_config_12_strings = [0, 0, 1, 0, 0, 2, 0, 0, 0, 1, 2, 3, 0, 2, 4]
+
                                         
+
+    cost_memo_table = pd.DataFrame({})
+    cost_memo_table["Parameter"] = "Project Location", "Project Name", "Required Power (MW)", \
+                                      "Required Energy (MWh)", "Nameplate Energy (MWh)", "AC Usable Energy at POM (MWh)",\
+                                        "Aux Losses Included?", "Degradation to COD", "Qty Strings", "Qty Containers (12 Strings)", \
+                                        "Qty Containers (10 Strings)", "Qty Containers (8 Strings)", "Total Qty Containers", "Qty PCS", "PCS Model"
+
+    cost_memo_table["Value"] = proj_location, proj_name, '{:,.2f}'.format(power_req), '{:,.2f}'.format(energy_req), \
+                               '{:,.2f}'.format(batt_nameplate*optimized_number_of_stacks), '{:,.2f}'.format((float(power_energy_rte_table["Usable AC Energy at POM (MWh)"][0].replace(',', ''))*1000)), \
+                               "Yes", months_to_COD, optimized_number_of_stacks, container_config_12_strings[i]*optimized_number_of_pcs, container_config_10_strings[i]*optimized_number_of_pcs, \
+                                container_config_8_strings[i]*optimized_number_of_pcs, optimized_number_of_containers, optimized_number_of_pcs, PCS_model
+    
+
 
 
     x_param = power_energy_table[:project_life+1].index.values.tolist()
@@ -462,8 +464,8 @@ def calculation(proj_location, proj_name, power_req, duration, number_cycles, po
 
     block_type = int(optimized_number_of_containers/optimized_number_of_pcs)
 
-    return fig, bol_config, aug_energy_table, power_energy_rte_table, financial_table, bill_of_materials, design_summary, losses_table, \
-        bol_design_summary, plot_title, y_axis_range, months_to_COD, block_type
+    return fig, bol_config, aug_energy_table, power_energy_rte_table, bill_of_materials, design_summary, losses_table, \
+        bol_design_summary, plot_title, y_axis_range, months_to_COD, block_type, cost_memo_table
 
 
 
@@ -631,6 +633,7 @@ html.Br(),
                     ], className='three columns'),
                 ], className='row'),
 
+
 html.Br(),
 
 html.Div([
@@ -659,6 +662,7 @@ html.Div([
     dash.dcc.Store(id = "stored_y_axis_range"),
     dash.dcc.Store(id = "stored_months_to_COD"),
     dash.dcc.Store(id = "stored_block_type"),
+    dash.dcc.Store(id = "stored_cost_memo_table"),
 ]),
 
 html.Br(),
@@ -674,11 +678,21 @@ html.Div([
 
 html.Br(),
 html.Br(),
+html.Br(),
 
+html.Div([
+    html.Div([html.H5("")], className='three columns'),
+    html.Button('Step 1 - Generate Cost Memo', id='generate_cost_memo', style=button_style, className='three columns'),
+    dcc.Loading(children = html.A('Step 2 - Download Cost Memo', id='download_cost_memo', href='', \
+                                  className='three columns', style= {"textAlign":"center", 'border-radius': '4px', \
+                                                                     'border': '1px solid navy','background-color': 'rgb(127, 81, 185)',\
+                                                                          'color': 'white', 'height': '38px', 'padding': '8px'}), type="circle")
+        ]),
+
+html.Br(),
+html.Br(),
 
 ])
-
-
 
 # Define callback to update the output
 
@@ -687,7 +701,6 @@ html.Br(),
     Output('bol_config', 'children'),
     Output('aug_energy_table', 'children'),
     Output('power_energy_rte_table', 'children'),
-    # Output('financial_table', 'children'),
     Output('stored_energy_plot', 'data'),
     Output('stored_bill_of_materials', 'data'),
     Output('stored_design_summary', 'data'),
@@ -699,6 +712,7 @@ html.Br(),
     Output('stored_y_axis_range', 'data'),
     Output('stored_months_to_COD', 'data'),
     Output('stored_block_type', 'data'),
+    Output('stored_cost_memo_table', 'data'),
     Output('generate_sizing', 'n_clicks'),
     [Input('inp_projloct', 'value'),
      Input('inp_projnm', 'value'),
@@ -719,7 +733,7 @@ def update_output(proj_location, proj_name, power_req, duration, number_cycles, 
     
     if n_clicks:
     
-        fig, bol_config, aug_energy_table, power_energy_rte_table, financial_table, bill_of_materials, design_summary, losses_table, bol_design_summary, plot_title, y_axis_range, months_to_COD, block_type = calculation(proj_location, proj_name, power_req, duration, number_cycles, point_of_measurement, RMU_Required, PF_required_at_POM, max_site_temp, oversize_required, project_life, number_of_augmentations, flat_guarantee)
+        fig, bol_config, aug_energy_table, power_energy_rte_table, bill_of_materials, design_summary, losses_table, bol_design_summary, plot_title, y_axis_range, months_to_COD, block_type, cost_memo_table = calculation(proj_location, proj_name, power_req, duration, number_cycles, point_of_measurement, RMU_Required, PF_required_at_POM, max_site_temp, oversize_required, project_life, number_of_augmentations, flat_guarantee)
     
         def table_format(table):
             return dash.dash_table.DataTable(table.to_dict('records', index=True), 
@@ -767,8 +781,6 @@ def update_output(proj_location, proj_name, power_req, duration, number_cycles, 
         
         power_energy_rte_dict = table_format(power_energy_rte_table)
 
-        # financial_table_dict = table_format(financial_table)
-
         fig_stored = fig
 
         bill_of_materials_stored = bill_of_materials.to_dict()
@@ -783,10 +795,11 @@ def update_output(proj_location, proj_name, power_req, duration, number_cycles, 
 
         power_energy_rte_table_stored = power_energy_rte_table.to_dict()
 
+        cost_memo_table_stored = cost_memo_table.to_dict()
 
         n_clicks = 0
         return fig, bol_config, aug_energy_dict, power_energy_rte_dict, fig_stored, bill_of_materials_stored, design_summary_stored, \
-    losses_table_stored, bol_design_summary_stored, aug_energy_table_stored, power_energy_rte_table_stored, plot_title, y_axis_range, months_to_COD, block_type, n_clicks
+    losses_table_stored, bol_design_summary_stored, aug_energy_table_stored, power_energy_rte_table_stored, plot_title, y_axis_range, months_to_COD, block_type, cost_memo_table_stored, n_clicks
 
     else:
         raise PreventUpdate
@@ -812,7 +825,6 @@ Output('generate-pdf-button', 'n_clicks'),
   Input('stored_y_axis_range', 'data'),
   Input('stored_months_to_COD', 'data'),
   Input('stored_block_type', 'data'),
-
  ]
 )
 
@@ -1177,6 +1189,190 @@ def update_pdf(n_clicks ,proj_location, proj_name, power_req, duration, project_
         # If button is not clicked, do nothing
         pdf_file = ''
     return pdf_file, n_clicks
+
+
+@app.callback(
+Output('download_cost_memo', 'href'),
+Output('generate_cost_memo', 'n_clicks'),
+ [Input('generate_cost_memo', 'n_clicks'),
+  Input('stored_cost_memo_table', 'data'),
+  Input('inp_projloct', 'value'),
+  Input('inp_projnm', 'value'),
+  Input('inp_projsize', 'value'),
+  Input('ddn_duration', 'value')
+ ]
+)
+
+def update_cost_memo(n_clicks, cost_memo_table, proj_location, proj_name, power_req, duration):
+    
+    def create_cost_memo(cost_memo_table, proj_location, proj_name, power_req, duration):
+
+        # Define Colors 
+        prevalon_lavendar = colors.Color(220/256,207/256,235/256)
+        prevalon_purple = colors.Color(127/256,81/256,185/256)
+        # Create a list to hold the contents of the PDF
+        content = []
+
+        # Define styles
+        styles = getSampleStyleSheet()
+        style_normal = styles["Normal"]
+
+        # Define header and footer function with image from URL
+        def header(canvas, doc):
+            
+            canvas.saveState()
+            img = Image.open('Prevalon Logo.jpg')
+            img_reader = ImageReader(img)
+            
+            header_left_size = 1*inch
+            canvas.drawImage(img_reader, 25, doc.bottomMargin + doc.height + doc.topMargin - header_left_size, width=header_left_size*1.827852998065764, height=header_left_size)  # Adjust image dimensions and position as needed
+
+            # Define the text to be wrapped
+            text = "400 International Parkway, Suite 200, Heathrow, FL 32746"
+
+            # Set the position and dimensions for drawing the text
+            width = 150
+            x = doc.leftMargin + doc.width + doc.rightMargin - width - 25
+            y = doc.bottomMargin + doc.height + doc.topMargin - 25
+            
+            line_spacing = 12
+
+            # Set font size and color
+            canvas.setFont("Helvetica", 8)  # Set font to Helvetica with size 12
+            canvas.setFillColorRGB(0, 0, 0)  # Set fill color to red (RGB: 1, 0, 0)
+
+            # Split the text into lines based on the width
+            lines = []
+            current_line = ''
+            for word in text.split():
+                
+                if canvas.stringWidth(current_line + ' ' + word) < width:
+                    current_line += ' ' + word
+                else:
+                    lines.append(current_line.strip())
+                    current_line = word
+            lines.append(current_line.strip())
+
+            # Draw each line individually
+            for line in lines:
+                text_width = canvas.stringWidth(line)
+                x_adjusted = x + (width - text_width)  # Adjust x position for right indentation
+                canvas.drawString(x_adjusted, y, line)
+                y -= line_spacing
+
+            # Set font size and color
+            canvas.setFont("Helvetica", 8)  # Set font to Helvetica with size 12
+            canvas.setFillColorRGB(84/256, 50/256, 122/256)  # Set fill color to red (RGB: 1, 0, 0)
+
+            y -= line_spacing
+
+            canvas.drawString(x_adjusted, y, "PrevalonEnergy.com")
+            canvas.restoreState()
+
+            # Footer
+
+            canvas.saveState()
+
+            footer_x = 0
+            footer_y = 25
+            canvas.setFillColorRGB(252/256, 215/256, 87/256)
+
+            # Set border color to transparent
+            canvas.setStrokeColorRGB(1, 1, 1, 0)  # Set border color to transparent (RGB: 1, 1, 1) and alpha (opacity) to 0
+            canvas.setFont("Helvetica", 12)
+
+            canvas.rect(footer_x, footer_y, 25, 25, fill=1)
+
+            page_num = canvas.getPageNumber()
+
+            canvas.setFillColorRGB(0, 0, 0)
+            canvas.drawString(25, 25+6, " %d" % page_num)
+
+            canvas.restoreState()
+
+
+        cost_memo_pdf = "Cost Memo " + str(proj_name) + ", " + str(proj_location) + ", "+ str('{:,.2f}'.format(power_req)) + "MW_"+ str('{:,.2f}'.format(power_req*duration)) + "MWh.pdf"
+
+        # Create a PDF document
+        doc = SimpleDocTemplate(
+            cost_memo_pdf,
+            pagesize=letter,
+        )
+
+
+        cost_memo_table = pd.DataFrame.from_dict(cost_memo_table)
+
+        cost_memo_table_data = []
+        cost_memo_table_data.append(cost_memo_table.columns.tolist())
+        for i in cost_memo_table.values.tolist():
+            cost_memo_table_data.append(i)
+
+        # print(cost_memo_table_data)
+        
+        # Add content to Technical Proposal
+        # Add title
+        title_text = str(proj_name) + ", " + str(proj_location) + ", "+ str('{:,.2f}'.format(power_req)) + "MW/"+ str('{:,.2f}'.format(power_req*duration)) +"MWh Battery Energy Storage System"
+        
+        title_paragraph_style = ParagraphStyle("title", fontSize = 14, fontName = "Helvetica-Bold", leading = 15)
+        section_paragraph_style = ParagraphStyle("section", fontSize = 12, fontName = "Helvetica-Bold")
+
+        # Define styles
+
+        def table_styles(table): 
+            table_style = [ ('BACKGROUND', (0, 0), (-1, 0), prevalon_purple), # Header Background
+                            ('TEXTCOLOR', (0, 0), (-1, 0), colors.white), # Header Text Color
+                            ('ALIGN', (0, 0), (-1, 0), 'CENTER'), # Allign Header Text Center
+                            ('FONTNAME', (0, 0), (-1, 0), 'Helvetica-Bold'), # Header Text Bold
+                            ("FONTSIZE", (0, 0), (-1, 0), 8),  # Header Font Size
+                            ('BOTTOMPADDING', (0, 0), (-1, 0), 3),  # Header Bottom Padding
+                            
+                            ('FONTNAME', (0, 1), (-1, -1), 'Helvetica'), # Table Font
+                            ("FONTSIZE", (0, 1), (-1, -1), 8),   # Table Font Size
+                            ('TEXTCOLOR', (0, 1), (-1, -1), colors.black), # Table Text Color
+                            ('ALIGN', (1, 1), (-1, -1), 'LEFT'), # Allign Table Text Center
+                        
+                            ('VALIGN', (0, 0), (-1, -1), 'MIDDLE'), # Table Text Color
+                            ('BOX', (0, 0), (-1, -1), 1, colors.black),
+                            ('WORDWRAP', (0, 0), (-1, -1)), 
+                            ]
+            
+            # Set different background colors for odd and even rows
+            for i in range(1, len(table)):
+                if i % 2 == 0:
+                    table_style.append(("BACKGROUND", (0, i), (-1, i), prevalon_lavendar))  # Light blue background color for even rows
+                else:
+                    table_style.append(("BACKGROUND", (0, i), (-1, i), colors.white))  # Light cyan background color for odd rows
+            
+            # Set BOX for columns
+            for i in range(len(table[0])):
+                table_style.append(("BOX", (i, 0), (i, -1), 0.1, colors.black))  # Light cyan background color for odd rows
+            
+            return table_style
+
+        title_paragraph = Paragraph(title_text, title_paragraph_style)
+
+        content.append(title_paragraph)
+
+        content.append(Paragraph("<br/><br/>", style_normal))
+        
+        table = Table(cost_memo_table_data)
+        table_style = table_styles(cost_memo_table_data)
+        table.setStyle(TableStyle(table_style))
+        content.append(table)
+        
+        doc.build(content, header, header)
+        # Return the URL for the download link
+        return cost_memo_pdf
+
+    if n_clicks:
+        # Generate PDF
+        cost_memo_pdf = '/download/{}'.format(create_cost_memo(cost_memo_table, proj_location, proj_name, power_req, duration))
+        n_clicks = 0
+
+    else:
+        # If button is not clicked, do nothing
+        cost_memo_pdf = ''
+    return cost_memo_pdf, n_clicks
 
 
 @app.server.route('/download/<path:path>')
